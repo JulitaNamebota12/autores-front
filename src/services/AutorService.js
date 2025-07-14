e3467c2e5b9df847a2e7caaafd4a3d261dec57bd
JulitaNamebota12 <julioppk27@gmail.com>
2025-07-14 13:09:58 -0500

    Update AutorService.js

--- src/services/AutorService.js
+++ src/services/AutorService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API_URL = 'https://autores-yopl.onrender.com/';
+const API_URL = 'https://autores-yopl.onrender.com/api/autores';
 
 export const getAutores = () => axios.get(API_URL);
